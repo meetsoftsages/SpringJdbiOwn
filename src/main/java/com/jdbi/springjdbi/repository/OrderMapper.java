6e1986fe6a9c757a57b153166af3591a0696b4df
Meet Patel <meet1999.patel@gmail.com>
2021-12-22 18:17:05 +0530

    Initial commit

---
+++ src/main/java/com/jdbi/springjdbi/repository/OrderMapper.java
@@ -0,0 +1,16 @@
+package com.jdbi.springjdbi.repository;
+
+import com.jdbi.springjdbi.domain.Order;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class OrderMapper implements RowMapper<Order> {
+
+    @Override
+    public Order map(ResultSet rs, StatementContext ctx) throws SQLException {
+        return new Order(rs.getInt("id"),rs.getString("order_name"),rs.getInt("quantity"),rs.getInt("amount"));
+    }
+}
